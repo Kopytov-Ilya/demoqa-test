befd53153172f9646eed14fa0cd237c161c6c140
ILYA KOPYTOV <kopytov.ilya.rus@mail.ru>
2023-01-08 22:11:05 +0300

    Added tests

---
+++ src/test/java/studentRegistrationForm.java
@@ -0,0 +1,85 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class studentRegistrationForm {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        //Configuration.holdBrowserOpen = true;
+    }
+    @Test
+    void successfulFilledForm() {
+        String firstName = "Test";
+        String lastName = "Testov";
+        String email = "Test@mail.ru";
+        String phoneNumber = "1234567890";
+        String currentAddress = "Test Federation, TestTown";
+        String subject = "Computer Science";
+        String gender = "Male";
+        String dateOfBirth = "18 February,1994";
+        String hobbies = "Sports, Reading, Music";
+        String picture = "ForTest.png";
+        String address = "Test Federation, TestTown";
+        String stateAndCity = "NCR Delhi";
+
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#userNumber").setValue(phoneNumber);
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("February");
+        $(".react-datepicker__year-select").selectOption("1994");
+        $(".react-datepicker__day--018").click();
+        $("#subjectsInput").setValue(subject).pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+        $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#uploadPicture").uploadFromClasspath("ForTest.png");
+        $("#currentAddress").setValue(currentAddress);
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
+
+        $(".modal-content").shouldBe(visible);
+        $(".modal-header").shouldHave(text("Thanks for submitting the form"));
+        $(".modal-body").shouldHave(text(firstName),
+                text(lastName), text(email), text(phoneNumber),
+                text(currentAddress), text(subject), text(gender),
+                text(dateOfBirth), text(hobbies), text(picture),
+                text(address), text(stateAndCity));
+    }
+
+
+
+
+
+    @Test
+    void unsuccessfulFilledForm() {
+
+        open("/automation-practice-form");
+        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+
+        $("#firstName").setValue("Test");
+        $("#submit").click();
+
+        $(".modal-content").shouldNotBe(visible);
+    }
+}
